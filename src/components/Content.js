8b3b49f20e3da5b351d9ad8b88d03fcd70f2bb9d
Mukul Mahajan <mukul.mahajan@internshala.com>
2024-01-22 23:09:22 +0530

    state in functional component

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,27 +1,37 @@
 import React from "react";
+import { useState } from "react";
 
 function Content(){
-    let name = 'Mukul Mahajan';
 
-    function call(){
-        name = "Internshala";
-        alert(name);
-    }
-    function callagain(){
-        // name = "Internshala";
-        alert(name);
-        console.log(name);
+    let [data, setState] = useState({
+        id: 12712,
+        name: "mukul",
+        company: "Internshala",
+        ctc : 5.4,
+    });
+    
+    function update(){
+
+        const obj = {
+            id: 754,
+            name: "kriti",
+            company: "Entigrity",
+            ctc : 3.2,
+        };
+
+        setState(obj);
     }
 
     return (
         <div>
-            <button onClick={call}>
+            <h1>{data.id}</h1>
+            <h1>{data.name}</h1>
+            <h1>{data.company}</h1>
+            <h1>{data.ctc}</h1>
+            <br />
+            <button onClick={update}>
                 Click Me
             </button>
-            <button onClick={callagain}>
-                Click Me again
-            </button>
-            <h2>{name}</h2>
         </div>
     );
 }
